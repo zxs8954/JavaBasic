ea65efd03718f83fcb9780b6e8b48b060521b552
zxs8954 <15547929160@163.com>
2020-04-22 20:12:17 +0800

    servlet

--- javaweb-02-servlet/servlet-02/src/main/java/com/kuang/servlet/HelloServlet.java
+++ javaweb-02-servlet/servlet-02/src/main/java/com/kuang/servlet/HelloServlet.java
@@ -1,5 +1,6 @@
 package com.kuang.servlet;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,14 +11,24 @@ import java.io.PrintWriter;
 public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //this.getInitParameter()  初始化参数
+        //this.getServletConfig()   Servlet配置
+        //this.getServletContext()  Servlet上下文
+        ServletContext i=this.getServletContext();
+        String username="张雪松";//数据
+        i.setAttribute("username",username);//讲一个数据保存在ServletContext中
+
+
+        System.out.println("hello");
         resp.setContentType("text/html");
         resp.setCharacterEncoding("utf-8");
-        PrintWriter writer=resp.getWriter();
+        PrintWriter writer = resp.getWriter();
         writer.println("可以了吧");
+        writer.println("<html><script>alert(\"恭喜恭喜\")</script></html>");
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        resp.getWriter().print("1231312");
     }
 }
